c775d4a40762a992f74e7175dc9aaa7f652bd27e
Mukul Anand <147015644+anand-mukul@users.noreply.github.com>
2024-02-26 04:08:11 +0530

    back to previous base

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.css";
 import "./styles/tailwind.css";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById("root"),
 );
